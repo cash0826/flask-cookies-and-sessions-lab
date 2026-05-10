05e025716d6df64358e7dcc9c600d5145c13f832
Melanie Rodriguez <melarodrigg@gmail.com>
2026-05-10 16:56:02 -0600

    Removed CORS import

--- server/app.py
+++ server/app.py
@@ -2,12 +2,10 @@
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
-from flask_cors import CORS
 
 from models import db, Article, User, ArticleSchema, UserSchema
 
 app = Flask(__name__)
-CORS(app)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
